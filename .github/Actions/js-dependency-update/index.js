ce1d9f3cee1dc3be3a7bcc358b7527e75443c9f2
vickyli5210 <viky.li.0129@gmail.com>
2026-03-06 21:29:45 +0000

    Vendor node_modules for js-dependency-update
    Bundle JavaScript dependencies for the js-dependency-update GitHub Action and update the action entrypoint. Adds many @actions, @octokit and related packages under .github/Actions/js-dependency-update/node_modules and updates index.js to use the bundled modules so the action can run without installing external dependencies.

--- .github/Actions/js-dependency-update/index.js
+++ .github/Actions/js-dependency-update/index.js
@@ -1,6 +1,21 @@
 const core = require('@actions/core');
 
 async function run(){
+    /*
+    1. Parse inputs:
+        1.1 base-branch from which to check for update
+        1.2 target-branch to create for the PR for update
+        1.3 GitHub token to use for creating the PR for authentication purpose
+        1.4 working directory to run npm commands for checking and updating dependencies
+    2. Execute the npm update within the working directory
+    3. Check if there are changes after the npm update in package*.json files, 
+    4.if there is update, 
+        4.1 add and commit files to the target branch
+        4.2 create a pr to the base-branch using the octokit API
+    5if not, then exit the action 
+
+
+        */
     core.info('Running JavaScript dependency update action...');
 }
 run()
